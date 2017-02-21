8fd1d6c6a567e208ca3e9958ee347e3ed2c01469
Luis Benitez <luisbttf@gmail.com>
2017-02-21 11:15:07 -0600

    initial project

---
+++ assets/js/functions.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.fade').slick({
+		autoplay: true,
+		arrows: false,
+	  dots: false,
+	  infinite: true,
+	  speed: 1000,
+	  fade: true,
+	  cssEase: 'linear',
+	});
+});
